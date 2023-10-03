0d12171de663deb6b2daa8c5dfd9fc5148822d14
Your Name <you@example.com>
2023-10-03 20:54:28 +0500

    revork

---
+++ bot_send.py
@@ -0,0 +1,36 @@
+import telebot
+import time
+import googlesheets
+
+bot = telebot.TeleBot('6567697373:AAFYnPXktIQRVOWppXOzGfGbMY_65TI57FM')
+
+chat_id = '302137006'
+response = googlesheets.get_comment()
+headers = response[0]
+record = response[1]
+
+previous_message = None
+
+def send_data_to_telegram():
+    global previous_message
+
+    message = ""
+    for header, value in zip(headers[2:-1], record[2:-1]):
+        message += f"{header} {value}\n"
+    message = message.replace("::", ":").replace(": :", ":").strip()
+
+    if message != previous_message:
+        bot.send_message(chat_id, message)
+        previous_message = message
+
+while True:
+    send_data_to_telegram()
+    time.sleep(300)
+
+
+
+
+
+
+
+
---
+++ googlesheets.py
@@ -0,0 +1,35 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Путь к JSON-ключу, который вы скачали
+json_keyfile = 'json.json'
+
+# Устанавливаем область видимости и аутентифицируемся
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name(json_keyfile, scope)
+gc = gspread.authorize(credentials)
+
+def get_comment():
+    # Открываем таблицу по её названию
+    worksheet = gc.open('ОС Админа').sheet1
+
+    data = worksheet.get_all_values()
+    # print(data)
+    return data
+
+
+
+# headers = data[0]
+# record = data[1]
+
+# for header, value in zip(headers[2:-1], record[2:-1]):
+#     print(header, value)
+# pairs = [(header, value) for header, value in zip(headers[2:-1], record[2:-1])
+
+
+
+
+
+
+
+
