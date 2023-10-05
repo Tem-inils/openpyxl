9a91a7fe408725861eff1c9698243d1c62810da2
Your Name <you@example.com>
2023-10-05 21:42:21 +0500

    asdalskd

--- database.py
+++ database.py
@@ -1,6 +1,24 @@
+import json
 import sqlite3
+import googlesheets as google
 
 connection = sqlite3.connect('data.db')
 sql = connection.cursor()
 
-sql.connection('CREATE TABLE IF NOT EXISTS admin_info (upload_time, user_mail, user_name, user_course, lesson_time, info_admin)')
+sql.execute('CREATE TABLE IF NOT EXISTS admin_info (id INTEGER, save_text TEXT);')
+
+
+def save_comment():
+    connection = sqlite3.connect('data.db')
+    sql = connection.cursor()
+    data = google.get_comment()[-1]
+    data_list = json.dumps(data)
+    sql.execute('INSERT INTO admin_info (save_text) VALUES (?);', (data_list,))
+    connection.commit()
+    connection.close()
+
+
+def check_comment():
+    pass
+
+save_comment()
